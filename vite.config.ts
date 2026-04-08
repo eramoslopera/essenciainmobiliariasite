d481bd4f7940028259753311885feec365053814
Esteban Ramos Lopera <destebanrl@MacBook-Pro-de-Esteban-2.local>
2026-04-08 18:06:12 +0200

    chore(build): increase chunkSizeWarningLimit to fix Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig(({ mode }) => {
       }
     },
     plugins: [react()],
+    build: {
+      chunkSizeWarningLimit: 2000
+    },
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
